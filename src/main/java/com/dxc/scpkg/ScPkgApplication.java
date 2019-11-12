7e9095ff1548325db3c0ce54a0d6c12f5eef58b3
jason0389 <747643480@qq.com>
2019-11-12 16:05:43 +0800

    Initial commit

---
+++ src/main/java/com/dxc/scpkg/ScPkgApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.scpkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScPkgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScPkgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dxc/scpkg/ScPkgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.scpkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScPkgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
